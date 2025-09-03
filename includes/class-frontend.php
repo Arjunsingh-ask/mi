8403d39647c9f0ec4f59918cc265b2c973090247
Arjunsingh-ask <avanit.dwivedi@gmail.com>
2025-09-03 18:39:31 +0530

    Update class-frontend.php

--- includes/class-frontend.php
+++ includes/class-frontend.php
@@ -3,15 +3,13 @@ if (!defined('ABSPATH')) exit;
 
 class AISEO_Frontend {
     public function __construct() {
-        add_action('wp_head', [$this, 'output_meta']);
+        add_action('wp_head', [$this, 'meta']);
     }
-
-    public function output_meta() {
+    public function meta() {
         if (!is_singular()) return;
         global $post;
-
         $title = get_post_meta($post->ID, '_aiseo_meta_title', true) ?: get_the_title($post);
-        $desc  = get_post_meta($post->ID, '_aiseo_meta_description', true) ?: wp_trim_words(strip_tags($post->post_content), 25);
+        $desc  = get_post_meta($post->ID, '_aiseo_meta_description', true) ?: wp_trim_words(wp_strip_all_tags($post->post_content), 28);
         $focus = get_post_meta($post->ID, '_aiseo_focus_keyword', true);
 
         echo '<title>'.esc_html($title).'</title>'."\n";
